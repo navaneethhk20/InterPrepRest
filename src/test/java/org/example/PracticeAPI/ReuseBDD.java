89182d26af80191581e0e310a66ea0dc0b4211b0
navaneethhhk <hknavaneeth@gmail.com>
2025-06-29 20:20:26 +0530

    2906

--- src/test/java/org/example/PracticeAPI/ReuseBDD.java
+++ src/test/java/org/example/PracticeAPI/ReuseBDD.java
@@ -1,4 +1,23 @@
 package org.example.PracticeAPI;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.when;
+
 public class ReuseBDD {
+    RequestSpecification rs;
+    Response r;
+    ValidatableResponse vr;
+
+    @Test
+    public void positive(){
+        rs = RestAssured.given();
+                rs.baseUri("https:// api.zippopotam.us").basePath("/IN/573129");
+        r = rs.when().log().all().get();
+        vr = r.then().log().all().statusCode(200);
+    }
 }
--- src/test/java/org/example/PracticeAPI/TestPositivetest.java
+++ src/test/java/org/example/PracticeAPI/TestPositivetest.java
@@ -1,4 +1,17 @@
 package org.example.PracticeAPI;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+
 public class TestPositivetest {
+
+
+    @Test
+    public void testNOnBdd(){
+        String pincode5 = "560066";
+        RestAssured.given().baseUri("http://api.zippopotam.us")
+                .basePath("/IN/" + pincode5).when().log().all().get()
+                .then().log().all().statusCode(200);
+    }
 }
